62baeabfc16618b996cfa7fab450eee98704ceec
bt3gl <steinkirch@protonmail.com>
2024-03-14 19:53:04 +0100

    quick fixes

--- doordash-api-scrapper/setup.py
+++ doordash-api-scrapper/setup.py
@@ -5,7 +5,7 @@ setup(
     name='fud_report',
     version='1.0',
     description='Python implementation of the Yelp Fusion API and Doordash scrapping.',
-    author='Mia Steinkirch',
+    author='Mia Stein',
     packages=[
         'fud_report',
     ],
